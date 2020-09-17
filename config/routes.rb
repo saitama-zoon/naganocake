e28831d7199137d6b543b3ea304d8ac606a9017e
vagrant <vagrant@localhost.localdomain>
2020-09-17 10:06:28 +0000

    [Upsate]Categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   devise_for :customers
-<<<<<<< Updated upstream
   devise_for :admins
-=======
   devise_for :admins, controllers: {
   	sessions: 'admin/sessions'
   }
     resources :categories, only: [:index, :edit, :create, :update]
->>>>>>> Stashed changes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
