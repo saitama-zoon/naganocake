c07606d4866bb61945f092012d37df2db04b82c3
Totu1002_Mac <totu.enjoy.music@gmail.com>
2020-09-17 18:32:00 +0900

    [Add]Order/routes,models,contorollers

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,2 +1,35 @@
 class Customer::OrdersController < ApplicationController
+
+  def new
+    @order = Oder.new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def confirm
+  end
+
+  def session
+  end
+
+  def thank
+  end
+
+  private
+
+  def method_name
+  end
+
+  def method_name
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :orders, only:[:new, :create, :index, :show]
+    namespace :admins do
+      resource :orders, only:[:index, :show, :update]
+  end
+
+  get "orders/confirm" => "orders#confirm"
+  get "orders/thank" => "orders#thank",as: "thank"
+  post "orders/session" => "orders#session",as: "session"
 end
