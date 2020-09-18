710f3955c34d7680ccb1b1f2edcfdf57e7cfc334
vagrant <vagrant@localhost.localdomain>
2020-09-18 11:10:15 +0000

    sign_in Kura

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
- before_action :authenticate_customer!
+
+#サインイン後の遷移先指定
+protected
+ def after_sign_in_path_for(resource)
+   home_path
+ end
+ #サインアップ後の遷移先指定
+ def after_sign_up_path_for(resource)
+　　home_path
+ end
+ #ログアウト後の遷移先指定
+ def after_sign_out_path_for(resource)
+   home_path
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions', registrations: 'customers/registrations', passwords: 'customers/passwords'
+  }
   devise_for :admins, controllers: {
   	sessions: 'sessions'
   }
-
-  root 'homes#top'
- 
-  resources :orders, only:[:new, :create, :index, :show]
-  get "orders/confirm" => "orders#confirm",as: "confirm"
-  get "orders/thank" => "orders#thank",as: "thank"
-  post "orders/session" => "orders#session",as: "session"
   
+  root 'customers/homes#top'
+  
+
+    get 'home' => "customers/homes#top",as: 'home'
+    get 'homes/about' => "customers/homes#about",as: 'about'
+    resources :orders, only: [:new, :create, :index, :show]
+    get "orders/confirm" => "customers/orders#confirm",as: "confirm"
+    get "orders/thank" => "customers/orders#thank",as: "thank"
+    post "orders/session" => "customers/orders#session_a",as: "session"
+
+
+  devise_scope :customers do
+    delete 'customers/sign_out' => 'customers/sessions#destroy', as: 'destroy_customers_session'
+  end
+
   namespace :admins do
     resources :orders, only:[:index, :show, :update]
     resources :customers, only:[:index,:show,:edit,:update]
