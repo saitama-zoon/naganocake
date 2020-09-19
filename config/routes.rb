71889931b4d01b98dbfd34a4e09237025fc0a0de
vagrant <vagrant@localhost.localdomain>
2020-09-19 02:51:49 +0000

    [Update]route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,41 @@
 Rails.application.routes.draw do
-  devise_for :customers
+
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions', registrations: 'customers/registrations', passwords: 'customers/passwords'
+  }
+
   devise_for :admins, controllers: {
     sessions: 'sessions'
   }
 
-  root 'customers/homes#top'
+  scope module: 'customers' do
+    root 'homes#top'
+    get 'home' => "homes#top", as: 'home'
+    get 'home/about' => "homes#about", as: 'about'
+    resource :customers, only: [:show, :edit, :update]
+    get "/withdrawal" => "customers#withdrawal", as: "withdrawal"
+    # 退会画面の表示
+    patch "/withdeawal" => "customers#hide", as: "hide"
+    # 退会処理アクション
+    resources :'destinatons',only: [:index, :create, :edit, :update, :destroy]
+    resources :'products',only: [:index,:show]
+    resources :'cart_products', only: [:index, :create, :update, :destroy]
+    resources :'orders', only: [:new, :create, :index, :show]
+    get "orders/confirm" => "orders#confirm", as: "confirm"
+    get "orders/thank" => "orders#thank", as: "thank"
+    post "orders/session" => "orders#session", as: "session"
+  end
 
-  resources :orders, only:[:new, :create, :index, :show]
-  get "orders/confirm" => "orders#confirm",as: "confirm"
-  get "orders/thank" => "orders#thank",as: "thank"
-  post "orders/session" => "orders#session",as: "session"
+  devise_scope :customers do
+    delete 'customers/sign_out' => 'customers/sessions#destroy', as: 'destroy_customers_session'
+  end
 
-  	# deviseではデフォルトで設定されるURLが存在するためnamespaseには含まない
   namespace :admins do
-  	# namespaceでadmins内のルーティングを指定
-  	resources :customers, only:[:index, :show, :edit, :update]
-    resources :products, only:[:index, :show, :new, :create, :edit, :update]
+    resources :orders, only: [:index, :show, :update]
+  	resources :customers, only: [:index, :show, :edit, :update]
+    resources :products, only: [:index, :show, :new, :create, :edit, :update]
     resources :categories, only: [:index, :edit, :create, :update]
+    resources :order_products, only: [:update]
   	get 'home' => "homes#home", as: 'home'
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
