7822fac080ea64efbd7e965b543990dfe5455dbd
vagrant <vagrant@localhost.localdomain>
2020-09-22 08:01:45 +0000

    [Add]order function

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,17 @@ class Order < ApplicationRecord
 
   has_many :products, :through => :order_products
 
+  enum payment_method: {
+    Not_set: 0,
+    Bank_transfer: 1,
+    Credit_card: 2
+  }
 
+  enum order_status: {
+    Waiting_for_payment: 0,
+    Payment_confirmation: 1,
+    In_production: 2,
+    Ready_to_ship: 3,
+    Sent: 4
+  }
 end
