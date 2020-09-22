2229e8e5a834a3d7c79a61bda7c6c6afb2ea735f
Masamitsu0922 <aaamt2209@gmail.com>
2020-09-22 06:20:31 +0000

    [Update]iroiro

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   scope module: 'customers' do
     resource :customer, only:[:show, :edit, :update]
     get 'customer/withdrawl'=> "customers#withdrawl"
-    patch 'customer/withdrawl' => "customers#hide"    
-    resources :destinatons,only:[:index, :create, :edit, :update, :destroy]
+    patch 'customer/withdrawl' => "customers#hide"
+    resources :destinations,only:[:index, :create, :edit, :update, :destroy]
     resources :products,only:[:index,:show]
     resources :cart_products, only:[:index, :update, :destroy]
     post "cart_product/:id" => "cart_products#create",as: "cart_products_create"
