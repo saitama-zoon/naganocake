8614357ff974df387f8568cfdcea9ea3d1ad50a3
vagrant <vagrant@localhost.localdomain>
2020-09-23 09:40:17 +0000

    Add top kura

--- app/controllers/customers/homes_controller.rb
+++ app/controllers/customers/homes_controller.rb
@@ -1,6 +1,7 @@
 class Customers::HomesController < ApplicationController
  def top
-
+ 	@products = Product.page(params[:page]).per(4)
+ 	@categories = Category.all
  end
  def about
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd6cd6245d39b9f9d281d0a483536b2df95e7e7ddc9b9fe106a1daa5b561856532d62893f8c5343675fb4bb5260e735c90f053ab32fd5267864cf72fbe1fecab9'
