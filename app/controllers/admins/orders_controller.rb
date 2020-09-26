b5d5c8099930ce2de223e90c11cb080256fccfa6
vagrant <vagrant@localhost.localdomain>
2020-09-26 08:52:03 +0000

    [Upadte]admins/orders

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -13,6 +13,32 @@ class Admins::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
+
+    #以下、文字列で値を取得する...
+    @order_str = params[:order][:order_status]
+    @product_str = params[:order][:product_status]
+
+    @order.order_products.each do |order_product|
+      case @order_str
+        #これでcase文は機能している...
+        when "Waiting_for_payment"
+          order_product.product_status = 0
+          @hello = "hello!"
+        when 1
+          order_product.product_status = 1
+        when 4
+          order_product.product_status = 4
+      end
+    end
+
+    case @product_str
+      when 2
+        @order.order_status = 2
+      when 3
+        @order.order_status = 3
+    end
+
+    binding.pry
     @order.update(order_params)
     redirect_to admins_order_path
   end
