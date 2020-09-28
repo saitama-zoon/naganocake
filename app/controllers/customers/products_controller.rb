ab922bc9a854f845e7709d467f0439dfc075f5a6
vagrant <vagrant@localhost.localdomain>
2020-09-28 12:51:40 +0000

    [Fiix]search function error

--- app/controllers/customers/products_controller.rb
+++ app/controllers/customers/products_controller.rb
@@ -21,23 +21,32 @@ class Customers::ProductsController < ApplicationController
     @categories = Category.where(is_effective: "true")
     category = Category.find(params[:id])
     @title = category.name
-    #binding.pry
+    binding.pry
     render 'index'
   end
 
   def name_search
 
+    #ジャンルが非選択時、全てのジャンルを定義
     if params[:category_id] == ""
       category = Product.all
     elsif
-    effective_category = Product.includes(:category).where(categories: {is_effective: true})
-    category = effective_category.where(category_id: params[:category_id])
+      #販売中の商品のみ取得
+      effective_category = Product.includes(:category).where(categories: {is_effective: true})
+      #販売中かつ有効なジャンルのみ取得
+      category = effective_category.where(category_id: params[:category_id])
+    end
+    #販売中かつ有効なジャンルの商品より、指定された名称の商品を取得
+    #ここが空欄だとそのようなカラムは存在しないため、SQLエラーが出力されると思われる。
+    #product = category.where(['name LIKE?', "%#{params[:search]}%"])
+    if params[:search] == ""
+      product = category
+    else
+      product = category.where(['name LIKE?', "%#{params[:search]}%"])
     end
 
-    product = category.where(['name LIKE?', "%#{params[:search]}%"])
     product_on_sales = product.where(is_sale_status: "true")
     @products = product_on_sales.page(params[:page]).per(8)
-    #binding.pry
     @quantity = product_on_sales.count
     @categories = Category.where(is_effective: "true")
      @title = "検索結果"
